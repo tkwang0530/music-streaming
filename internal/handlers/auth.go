f8623c3de23cd8d907121606f31cf39fc4bc2f67
Tommy <tkwang0530@gmail.com>
2023-02-28 21:27:12 +0800

    store token in session and add logout route

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/tkwang0530/music-streaming/internal/config"
 	"golang.org/x/oauth2"
@@ -40,6 +41,16 @@ func (h *AuthHandler) Callback(c *gin.Context) {
 	// TODO: Save token to database or session
 	fmt.Println(token)
 
+	// Store token in session
+	session := sessions.Default(c)
+	session.Options(sessions.Options{MaxAge: 60 * 60 * 24, HttpOnly: true}) // 1 day
+	session.Set("token", token)
+	err = session.Save()
+	fmt.Println("err: ", err)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save session"})
+		return
+	}
 	c.JSON(http.StatusOK, gin.H{"message": "Successfully logged in"})
 }
 
@@ -51,4 +62,15 @@ func (h *AuthHandler) getOAuthConfig() *oauth2.Config {
 		Scopes:       []string{"openid", "profile", "email"},
 		Endpoint:     google.Endpoint,
 	}
+}
+
+func (h *AuthHandler) Logout(c *gin.Context) {
+	session := sessions.Default(c)
+	session.Delete("token")
+	if err := session.Save(); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save session"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Successfully logged out"})
 }
--- internal/routers/router.go
+++ internal/routers/router.go
@@ -2,16 +2,25 @@ package routers
 
 import (
 	"database/sql"
+	"encoding/gob"
 	"log"
 
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 	"github.com/tkwang0530/music-streaming/internal/config"
 	"github.com/tkwang0530/music-streaming/internal/handlers"
 	"github.com/tkwang0530/music-streaming/internal/repositories"
+	"golang.org/x/oauth2"
 )
 
 func SetupRouter(db *sql.DB) *gin.Engine {
+	gob.Register(&oauth2.Token{}) // Register oauth2.Token type with gob
+
 	r := gin.Default()
+	// Initialize session middleware
+	store := cookie.NewStore([]byte("secret"))
+	r.Use(sessions.Sessions("mysession", store))
 
 	// Load configuration
 	cfg, err := config.Load()
@@ -23,6 +32,7 @@ func SetupRouter(db *sql.DB) *gin.Engine {
 	oauth2Group := r.Group("/oauth2")
 	oauth2Group.GET("/google/login", oauthHandler.Login)
 	oauth2Group.GET("/google/callback", oauthHandler.Callback)
+	oauth2Group.GET("/google/logout", oauthHandler.Logout)
 
 	// Setup routes
 	songsRepo := repositories.NewSongRepository(db)
